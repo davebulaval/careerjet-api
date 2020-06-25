ca266b173e80846daf50393d3b2a842085a8ecdb
davebulaval <david.beauchemin.5@ulaval.ca>
2020-06-25 12:21:33 -0400

    better info

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
+import os
+
 from setuptools import setup
 
-setup(name='careerjet_api',
-      version='3.1',
-      description='Python interface to Careerjet\'s public search API',
-      url='https://github.com/davebulaval/careerjet-api',
-      author='Davebulaval',
-      # author_email='',
-      license='MIT',
-      packages=['careerjet_api'],
-      install_requires=['requests'],
-      zip_safe=False)
+current_file_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_readme():
+    readme_file_path = os.path.join(current_file_path, 'README.md')
+    with open(readme_file_path, 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def main():
+    readme = get_readme()
+    setup(name='careerjet_api',
+          version='3.1',
+          description='Python interface to Careerjet\'s public search API',
+          url='https://github.com/davebulaval/careerjet-api',
+          author='davebulaval',
+          # author_email='',
+          license='MIT',
+          packages=['careerjet_api'],
+          install_requires=['requests'],
+          long_description=readme,
+          long_description_content_type='text/markdown',
+          zip_safe=False)
